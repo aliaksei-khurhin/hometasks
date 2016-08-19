58f14d1365eb21c4afec5ae2c4afcf4c847b940f
Alex Hurgin <aliaksei_khurhin@epam.com>
2016-08-19 16:33:25 +0300

    2

--- 3.py
+++ 3.py
@@ -1,7 +1,5 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-for i in range (set(a)):
-    for j in range (set(b)):
-        if set(a)[i]==b[j]:
-            c +=a[i]
+c=list(set(a).intersection(set(b)))
+print (c)
